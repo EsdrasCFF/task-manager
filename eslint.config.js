84a7a1dc97b71a0dc981c67b222df097f77df2c2
Esdras Castro <ecff.dev@gmail.com>
2024-08-08 13:12:35 -0300

    chore: correct config files of eslint and lint-staged

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended, "prettier"],
+  extends: [js.configs.recommended, ...tseslint.configs.recommended, 'plugin:prettier/recommended'],
   files: ['**/*.{ts,tsx}'],
   ignores: ['dist'],
   languageOptions: {
