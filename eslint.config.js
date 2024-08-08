2168697b4a62e6e6dfeacf6c2eca6fccc99da7a0
Esdras Castro <ecff.dev@gmail.com>
2024-08-08 13:43:56 -0300

    feat: add prettier config and eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,10 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import prettier from 'eslint-config-prettier'
 
 export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
+  extends: [js.configs.recommended, ...tseslint.configs.recommended, prettier],
   files: ['**/*.{ts,tsx}'],
   ignores: ['dist'],
   languageOptions: {
