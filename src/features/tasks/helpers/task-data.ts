dc7892bc8f730086f785ab05a02229738235ae0c
Esdras Castro <ecff.dev@gmail.com>
2024-08-09 10:36:32 -0300

    feat: add task-data for test and styling

---
+++ src/features/tasks/helpers/task-data.ts
@@ -0,0 +1,44 @@
+export const tasksData = [
+  {
+    id: 1,
+    title: 'Fazer Compras',
+    description: 'Comprar os ingredientes para o jantas',
+    time: 'afternoon',
+    status: 'done',
+  },
+  {
+    id: 2,
+    title: 'Aula de Ingles',
+    description: 'Finalizar aula de inglês',
+    time: 'morning',
+    status: 'in_progress',
+  },
+  {
+    id: 3,
+    title: 'Reunião TOTVS',
+    description: 'Reunião com a Totvs, preparar material',
+    time: 'morning',
+    status: 'in_progress',
+  },
+  {
+    id: 4,
+    title: 'Aula de Ingles',
+    description: 'Finalizar aula de inglês',
+    time: 'afternoon',
+    status: 'done',
+  },
+  {
+    id: 5,
+    title: 'Devocinal',
+    description: 'Fazer devocional diario',
+    time: 'evening',
+    status: 'not_started',
+  },
+  {
+    id: 6,
+    title: 'Aula de Ingles',
+    description: 'Finalizar aula de inglês',
+    time: 'evening',
+    status: 'not_started',
+  },
+]
