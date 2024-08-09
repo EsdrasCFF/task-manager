ce3758ab8f9b1ef0da310575be8e143e8980958a
Esdras Castro <ecff.dev@gmail.com>
2024-08-09 10:02:38 -0300

    fix: renamed DayShift component to TaskSeparator

--- src/components/task-separator.tsx
+++ src/components/task-separator.tsx
@@ -5,7 +5,7 @@ type Props = {
   icon: LucideIcon
 }
 
-export function DayShift({ icon: Icon, title }: Props) {
+export function TaskSeparator({ icon: Icon, title }: Props) {
   return (
     <h3 className="flex w-full items-center border-b-[2.5px] border-lightGray pb-1 text-sm font-semibold text-textGray">
       <Icon size={20} className="mr-2" /> {title}
