030f881ddd9e7341f814a3734d509fb7cd8036c2
Esdras Castro <ecff.dev@gmail.com>
2024-08-09 11:44:31 -0300

    feat: add ExternalLink Icon on TaskItem

--- src/components/taks-item.tsx
+++ src/components/taks-item.tsx
@@ -1,4 +1,4 @@
-import { Check, LoaderCircle, X } from 'lucide-react'
+import { Check, ExternalLink, LoaderCircle, X } from 'lucide-react'
 import { TaskData } from '../features/tasks/helpers/task-data'
 import { twMerge } from 'tailwind-merge'
 
@@ -10,28 +10,32 @@ export function TaskItem({ task }: Props) {
   return (
     <div
       className={twMerge(
-        'flex w-full gap-3 rounded-lg p-2',
-        task.status === 'done' && 'bg-lightBlue text-darkBlue',
+        'flex w-full items-center justify-between rounded-lg p-2',
+        task.status === 'done' && 'bg-lightBlue text-primary',
         task.status === 'in_progress' && 'bg-lightYellow text-yellow',
-        task.status === 'not_started' && 'bg-lightGray text-darkBlue'
+        task.status === 'not_started' && 'bg-lightGray text-gray-600'
       )}
     >
-      <div
-        className={twMerge(
-          'flex size-6 items-center justify-center rounded-md text-white',
-          task.status === 'done' && 'bg-primary',
-          task.status === 'in_progress' && 'bg-darkYellow',
-          task.status === 'not_started' && 'bg-gray-400'
-        )}
-      >
-        {task.status === 'done' && <Check size={16} strokeWidth={3} />}
-        {task.status === 'in_progress' && (
-          <LoaderCircle size={16} strokeWidth={3} className="animate-spin" />
-        )}
-        {task.status === 'not_started' && <X size={16} strokeWidth={3} />}
+      <div className="flex items-center gap-3">
+        <button
+          className={twMerge(
+            'flex size-6 items-center justify-center rounded-md text-white',
+            task.status === 'done' && 'bg-primary',
+            task.status === 'in_progress' && 'bg-darkYellow',
+            task.status === 'not_started' && 'bg-gray-400'
+          )}
+        >
+          {task.status === 'done' && <Check size={16} strokeWidth={3} />}
+          {task.status === 'in_progress' && (
+            <LoaderCircle size={16} strokeWidth={3} className="animate-spin" />
+          )}
+          {task.status === 'not_started' && <X size={16} strokeWidth={3} />}
+        </button>
+
+        <p className="text-sm leading-none">{task.title}</p>
       </div>
 
-      <p className="text-sm">{task.title}</p>
+      <ExternalLink size={20} />
     </div>
   )
 }
