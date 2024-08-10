b4025bcace68cc69a701ca3ecdf510bd7e45d085
Esdras Castro <ecff.dev@gmail.com>
2024-08-10 16:42:20 -0300

    feat: add ENUM type to task stats

--- src/features/tasks/helpers/task-data.ts
+++ src/features/tasks/helpers/task-data.ts
@@ -1,9 +1,15 @@
+export enum TaskStatus {
+  DONE = 'done',
+  IN_PROGRESS = 'in_progress',
+  NOT_STARTED = 'not_started',
+}
+
 export interface TaskData {
   id: number
   title: string
   description: string
   time: 'afternoon' | 'morning' | 'evening'
-  status: 'done' | 'in_progress' | 'not_started'
+  status: TaskStatus
 }
 
 export const tasksData: TaskData[] = [
@@ -12,41 +18,41 @@ export const tasksData: TaskData[] = [
     title: 'Fazer Compras',
     description: 'Comprar os ingredientes para o jantas',
     time: 'afternoon',
-    status: 'done',
+    status: TaskStatus.DONE,
   },
   {
     id: 2,
     title: 'Aula de Ingles',
     description: 'Finalizar aula de inglês',
     time: 'morning',
-    status: 'in_progress',
+    status: TaskStatus.IN_PROGRESS,
   },
   {
     id: 3,
     title: 'Reunião TOTVS',
     description: 'Reunião com a Totvs, preparar material',
     time: 'morning',
-    status: 'in_progress',
+    status: TaskStatus.NOT_STARTED,
   },
   {
     id: 4,
     title: 'Aula de Ingles',
     description: 'Finalizar aula de inglês',
     time: 'afternoon',
-    status: 'done',
+    status: TaskStatus.DONE,
   },
   {
     id: 5,
     title: 'Devocinal',
     description: 'Fazer devocional diario',
     time: 'evening',
-    status: 'not_started',
+    status: TaskStatus.IN_PROGRESS,
   },
   {
     id: 6,
     title: 'Aula de Ingles',
     description: 'Finalizar aula de inglês',
     time: 'evening',
-    status: 'not_started',
+    status: TaskStatus.NOT_STARTED,
   },
 ]
