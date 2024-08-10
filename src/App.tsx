45402b64682a82fe917995962fc921a27ea3a2a0
Esdras Castro <ecff.dev@gmail.com>
2024-08-10 17:03:58 -0300

    feat: ensure toast is shown after task update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Toaster } from 'sonner'
 import { Sidebar } from './components/sidebar'
 import { Tasks } from './components/tasks'
 
 export default function App() {
   return (
     <div className="flex">
+      <Toaster />
       <Sidebar />
       <Tasks />
     </div>
