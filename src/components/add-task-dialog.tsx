396c1aeca9b98d5871b7755221136314082d90ea
Esdras Castro <ecff.dev@gmail.com>
2024-08-10 22:03:56 -0300

    feat: add transition-group on AddTaskDialog component

--- src/components/add-task-dialog.tsx
+++ src/components/add-task-dialog.tsx
@@ -1,4 +1,8 @@
+import './add-task-dialog.css'
+
+import { useRef } from 'react'
 import { createPortal } from 'react-dom'
+import { CSSTransition } from 'react-transition-group'
 
 import { Button } from './button'
 import { Input } from './input'
@@ -9,33 +13,56 @@ type Props = {
 }
 
 export function AddTaskDialog({ isOpen, handleCancelClick }: Props) {
-  if (!isOpen) return null
+  const nodeRef = useRef(null)
 
-  return createPortal(
-    <div className="fixed inset-0 flex h-screen w-screen items-center justify-center backdrop-blur-sm">
-      <div className="flex w-full max-w-[340px] flex-col items-center rounded-xl bg-white p-5 shadow">
-        <h1 className="text-xl font-semibold text-darkBlue">Nova Tarefa</h1>
-        <p className="text-sm text-textGray">Insira as informações abaixo</p>
+  return (
+    <CSSTransition
+      nodeRef={nodeRef}
+      in={isOpen}
+      timeout={600}
+      classNames="add-task-dialog"
+      unmountOnExit
+    >
+      <div>
+        {createPortal(
+          <div
+            className="fixed inset-0 flex h-screen w-screen items-center justify-center backdrop-blur-sm"
+            ref={nodeRef}
+          >
+            <div className="flex w-full max-w-[340px] flex-col items-center rounded-xl bg-white p-5 shadow">
+              <h1 className="text-xl font-semibold text-darkBlue">
+                Nova Tarefa
+              </h1>
+              <p className="text-sm text-textGray">
+                Insira as informações abaixo
+              </p>
 
-        <div className="mt-4 w-full space-y-4">
-          <Input placeholder="Título da tarefa" label="Título" id="title" />
-          <Input placeholder="Horário" label="Período" id="period" />
-          <Input
-            placeholder="Descreva a tarefa"
-            label="Descrição"
-            id="description"
-          />
+              <div className="mt-4 w-full space-y-4">
+                <Input
+                  placeholder="Título da tarefa"
+                  label="Título"
+                  id="title"
+                />
+                <Input placeholder="Horário" label="Período" id="period" />
+                <Input
+                  placeholder="Descreva a tarefa"
+                  label="Descrição"
+                  id="description"
+                />
 
-          <div className="flex gap-3">
-            <Button variant="secondary" onClick={handleCancelClick}>
-              {' '}
-              Cancelar{' '}
-            </Button>
-            <Button variant="primary"> Salvas </Button>
-          </div>
-        </div>
+                <div className="flex gap-3">
+                  <Button variant="secondary" onClick={handleCancelClick}>
+                    {' '}
+                    Cancelar{' '}
+                  </Button>
+                  <Button variant="primary"> Salvas </Button>
+                </div>
+              </div>
+            </div>
+          </div>,
+          document.body
+        )}
       </div>
-    </div>,
-    document.body
+    </CSSTransition>
   )
 }
