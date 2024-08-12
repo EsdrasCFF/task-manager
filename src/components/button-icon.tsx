898941badc607613cd79b03197ce6d23818ea0f0
Esdras Castro <ecff.dev@gmail.com>
2024-08-12 00:11:16 -0300

    feat: add Tailwind Variants on ButtonIcon component

--- src/components/button-icon.tsx
+++ src/components/button-icon.tsx
@@ -1,22 +1,31 @@
 import { LucideIcon } from 'lucide-react'
 import { ComponentProps } from 'react'
-import { twMerge } from 'tailwind-merge'
+import { tv } from 'tailwind-variants'
 
 interface ButtonProps extends ComponentProps<'button'> {
   title: string
   icon: LucideIcon
-  variant?: 'primary' | 'outline'
+  variant?: 'primary' | 'outline' | 'destructive'
+  className?: string
 }
 
-export function ButtonWithIcon({ icon: Icon, title, variant = 'primary', ...props }: ButtonProps) {
+export function ButtonWithIcon({ icon: Icon, title, variant, className, ...props }: ButtonProps) {
+  const button = tv({
+    base: `flex gap-1 rounded-md px-3 py-1 text-xs font-semibold transition hover:bg-opacity-60 ${className}`,
+    variants: {
+      variant: {
+        primary: 'bg-primary text-white',
+        outline: 'text-darkGray hover:bg-gray-200/40',
+        destructive: 'bg-danger text-white',
+      },
+    },
+    defaultVariants: {
+      variant: 'primary',
+    },
+  })
+
   return (
-    <button
-      className={twMerge(
-        'flex gap-1 rounded-md px-3 py-1 text-xs font-semibold transition hover:bg-opacity-60',
-        variant == 'primary' ? 'bg-primary text-white' : 'text-darkGray hover:bg-gray-200/40'
-      )}
-      {...props}
-    >
+    <button className={button({ variant })} {...props}>
       {title} <Icon size={16} />
     </button>
   )
