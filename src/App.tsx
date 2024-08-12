4e4fe0c3c876ea1ce9a41cf3c5583f682f863e2b
Esdras Castro <ecff.dev@gmail.com>
2024-08-12 18:06:21 -0300

    fix: moving ToasterProvider to main file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { Toaster } from 'sonner'
-
 import { Sidebar } from './components/sidebar'
 import { Tasks } from './components/tasks'
 
 export default function App() {
   return (
     <div className="flex">
-      <Toaster />
       <Sidebar />
       <Tasks />
     </div>
