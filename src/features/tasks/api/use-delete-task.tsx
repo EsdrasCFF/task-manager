cfecaff7416fd2c0d04a5602ee7e5bc8de0c6588
Esdras Castro <ecff.dev@gmail.com>
2024-08-13 13:43:14 -0300

    feat: add useDeleteTask mutation hook of RQ

---
+++ src/features/tasks/api/use-delete-task.tsx
@@ -0,0 +1,42 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { toast } from 'sonner'
+
+import { TaskData } from '../helpers/task-data'
+
+type RequestType = {
+  taskId: string
+}
+
+export function useDeleteTaks() {
+  const queryClient = useQueryClient()
+
+  const mutation = useMutation<TaskData, Error, RequestType>({
+    mutationFn: async (json) => {
+      const response = await fetch(`http://localhost:3000/tasks/${json.taskId}`, {
+        method: 'DELETE',
+      })
+
+      if (!response.ok) {
+        throw new Error('Failed to delete task!')
+      }
+
+      const data = await response.json()
+      return data
+    },
+    onMutate: (json) => {
+      queryClient.setQueryData(['tasks'], (prevTasks: TaskData[]) => {
+        const tasks = prevTasks.filter((task) => task.id != json.taskId)
+        return tasks
+      })
+    },
+    onError: () => {
+      queryClient.invalidateQueries({ queryKey: ['tasks'] })
+      toast.error('Erro ao deletar tarefa!')
+    },
+    onSuccess: () => {
+      toast.success('Tarega deletada com sucesso!')
+    },
+  })
+
+  return mutation
+}
